e884175c627ca50493c1c96b80d8f4218d0e485c
FangCheng666 <1356712403@qq.com>
2018-10-11 11:25:10 +0800

    修改bug

--- app/src/main/java/com/lzy/imagepickerdemo/ImagePickerActivity.java
+++ app/src/main/java/com/lzy/imagepickerdemo/ImagePickerActivity.java
@@ -24,7 +24,7 @@ import com.lzy.imagepicker.ImagePicker;
 import com.lzy.imagepicker.bean.ImageItem;
 import com.lzy.imagepicker.ui.ImageGridActivity;
 import com.lzy.imagepicker.view.CropImageView;
-import com.lzy.imagepickerdemo.imageloader.GlideImageLoader;
+import com.lzy.imagepicker.loader.GlideImageLoader;
 import com.lzy.imagepickerdemo.imageloader.PicassoImageLoader;
 import com.lzy.imagepickerdemo.imageloader.UILImageLoader;
 import com.lzy.imagepickerdemo.imageloader.XUtils3ImageLoader;
